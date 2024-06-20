a217930850df3b44959d767e27f479909c1c0d50
MrFutureBoss <146051100+MrFutureBoss@users.noreply.github.com>
2024-06-21 00:00:17 +0700

    oke

--- functions/index.js
+++ functions/index.js
@@ -5,43 +5,45 @@ import dotenv from "dotenv";
 import connectDB from "./helpers/init_mongodb.js";
 import UserRouter from "./router/User.js";
 import ProductRouter from "./router/Product.js";
-import cors from 'cors'
-const defaultroute = express.Router();
+import cors from 'cors';
 import serverless from 'serverless-http';
-dotenv.config();
-const PORT = process.env.PORT;
 
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, preflightContinue: true }));
-//Router:
+
 app.get("/", async (req, res) => {
     res.send("Hello Mai Tu");
 });
-app.use('/.netlify/functions/api', defaultroute)
+
 app.use('/user', UserRouter);
 app.use('/product', ProductRouter);
 
 app.use(async (req, res, next) => {
     next(createError.NotFound());
-})
+});
 
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        }
+    });
+});
 
-// app.use((err, req, res, next) => {
-//     res.status(err.status || 500);
-//     res.send({
-//         error: {
-//             status: err.status || 500,
-//             message: err.message,
-//         }
-//     });
-// });
+// If running on Netlify, don't listen on a port
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        connectDB();
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server running on port ${PORT}`);
-})
 export const handler = serverless(app);
